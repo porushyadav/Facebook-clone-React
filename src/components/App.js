1a2bf46b164ee591d1b5b494c21acd3bdc09c2a6
porushyadav <37874551+porushyadav@users.noreply.github.com>
2020-08-25 13:37:25 +0530

    Setup store for facebook-clone

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+class App extends React.Component {
+  render() {
+    return <div className="App">ddsds</div>;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as firebase from 'firebase';
-import 'firebase/firestore';
+import App from './components/App';
+import { configureStore } from './store';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBeLfzHtpudjlos8AOiyxPIT5rktHG_LXE",
-  authDomain: "cart-4c74f.firebaseapp.com",
-  databaseURL: "https://cart-4c74f.firebaseio.com",
-  projectId: "cart-4c74f",
-  storageBucket: "cart-4c74f.appspot.com",
-  messagingSenderId: "829713431522",
-  appId: "1:829713431522:web:0b44d496fde6bed45417bb"
-  
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const store = configureStore();
+console.log(store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
