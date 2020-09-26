cd16cb624b43ec40bbf7544d01d28244228985bc
porushyadav <37874551+porushyadav@users.noreply.github.com>
2020-09-26 19:51:36 +0530

    Updates

---
+++ precache-manifest.1d2449fafa10a4801962a11dced32a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae0604dae6701c2918388526c9decc0",
+    "url": "/Facebook-clone-React/index.html"
+  },
+  {
+    "revision": "7dfcafb15e61cc45d9ab",
+    "url": "/Facebook-clone-React/static/css/main.bf52887c.chunk.css"
+  },
+  {
+    "revision": "5c60e2628ab6e26bd02c",
+    "url": "/Facebook-clone-React/static/js/2.6148a3a6.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/Facebook-clone-React/static/js/2.6148a3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfcafb15e61cc45d9ab",
+    "url": "/Facebook-clone-React/static/js/main.79efe041.chunk.js"
+  },
+  {
+    "revision": "73a412738e87ad078f42",
+    "url": "/Facebook-clone-React/static/js/runtime-main.d7c2085e.js"
+  }
+]);
